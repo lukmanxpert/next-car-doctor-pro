20272aec4cf556fbf4b1daf2dfc5bd9a990a50bf
lukmanxpert <lukmanmiah2004@gmail.com>
2025-06-05 23:50:03 +0600

    added content to about us section

--- src/app/components/AboutUs.jsx
+++ src/app/components/AboutUs.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
 export default function AboutUs() {
-  return (
-    <div>AboutUs</div>
-  )
+    return (
+        <div className='flex justify-between items-center gap-4 mt-10'>
+            <div className='flex-1'>
+                <h1>heres something</h1>
+            </div>
+            <div className='flex-1 text-left'>
+                <h1 className='text-primary-100 font-semibold text-lg'>About Us</h1>
+                <h2 className='text-3xl font-bold'>We are qualified <br /> & of experience <br /> in this field</h2>
+                <p className='text-[#737373]'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                <p className='text-[#737373]'>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+            </div>
+        </div>
+    )
 }
