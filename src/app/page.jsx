da8739f403856cb5f76f13686403be389e208fa9
lukmanxpert <lukmanmiah2004@gmail.com>
2025-06-05 23:37:43 +0600

    added about us section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,11 @@
+import AboutUs from "./components/AboutUs";
 import Banner from "./components/Banner";
 
 export default function Home() {
   return (
-    <Banner />
+    <div>
+      <Banner />
+      <AboutUs />
+    </div>
   );
 }
