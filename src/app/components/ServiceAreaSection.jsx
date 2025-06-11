3cef65b416692720886f969d81b107b40a6d4d10
lukmanxpert <lukmanmiah2004@gmail.com>
2025-06-11 19:09:13 +0600

    connect to mongodb and get service data from mongodb

--- src/app/components/ServiceAreaSection.jsx
+++ src/app/components/ServiceAreaSection.jsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
-import services from "../../../public/services.json";
 import HeaderTitle from "../shared/HeaderTitle";
 import { FaArrowRight } from "react-icons/fa";
+import dbConnect from "../lib/dbConnect";
 
-export default function ServiceAreaSection() {
-    const data = services;
+export default async function ServiceAreaSection() {
+    const servicesCollection = dbConnect("services")
+    const data = await servicesCollection.find({}).toArray()
+    console.log(data);
     return (
         <div className="my-4">
             <HeaderTitle name={"Service"} title={"Our Service Area"} desc1={"the majority have suffered alteration in some form, by injected humour, or randomised"} desc2={"words which don't look even slightly believable."} />
@@ -13,7 +15,7 @@ export default function ServiceAreaSection() {
                     data.map((service) => {
                         return (
                             <div
-                                key={service._id}
+                                key={service.service_id}
                                 className="border border-[#E8E8E8] rounded p-4 w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-sm flex flex-col mx-auto"
                             >
                                 <div className="w-full h-40 sm:h-48 md:h-40 lg:h-56 relative mb-2">
---
+++ src/app/lib/dbConnect.js
@@ -0,0 +1,16 @@
+import { MongoClient } from "mongodb";
+import { ServerApiVersion } from "mongodb";
+
+const dbConnect = (collection) => {
+  const uri = process.env.db_uri;
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+  return client.db(process.env.db_user).collection(collection);
+};
+
+export default dbConnect;
