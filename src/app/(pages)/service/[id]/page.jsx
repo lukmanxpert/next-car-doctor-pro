c2f8181e5a2b658f00f59c9a29e9b07eff92a971
lukmanxpert <lukmanmiah2004@gmail.com>
2025-06-11 23:54:49 +0600

    added some styled at service details page

--- src/app/(pages)/service/[id]/page.jsx
+++ src/app/(pages)/service/[id]/page.jsx
@@ -1,6 +1,7 @@
 import dbConnect, { collectionNameObj } from "@/app/lib/dbConnect";
 import SharedBanner from "@/app/shared/SharedBanner";
 import { ObjectId } from "mongodb";
+import Image from "next/image";
 
 export default async function ServiceDetailsPage({ params }) {
     const { id } = await params;
@@ -12,8 +13,18 @@ export default async function ServiceDetailsPage({ params }) {
             <div>
                 <SharedBanner title={"Service Details"} route={"Home/Service Details"} />
             </div>
-            <div className="grid grid-cols-3 w-full h-52 mt-4">
-                <div className="col-span-2 bg-red-200"></div>
+            <div className="grid grid-cols-3 gap-4 w-full h-52 mt-4">
+                <div className="col-span-2">
+                    <div>
+                        <Image src={service.img} width={752} height={400} alt="details image"
+                            className="w-full rounded max-h-[400px] object-cover"
+                        />
+                    </div>
+                    <div>
+                        <h1 className="font-bold text-4xl text-black my-4">{service.title}</h1>
+                        <p className="text-base text-text-1">{service.description}</p>
+                    </div>
+                </div>
                 <div className="col-span-1 bg-red-400"></div>
             </div>
         </div>
