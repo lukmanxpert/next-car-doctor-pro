1806591fdecf94c429eb288d75e45b1d3be7ab5a
lukmanxpert <lukmanmiah2004@gmail.com>
2025-06-13 22:47:26 +0600

    functional login page

--- src/app/(pages)/(authenticaton)/login/components/LogInForm.jsx
+++ src/app/(pages)/(authenticaton)/login/components/LogInForm.jsx
@@ -1,67 +1,35 @@
 "use client"
-import React, { useState } from 'react'
 import Link from 'next/link'
-import { registerUser } from "../../../../actions/auth/registerController"
-import toast from 'react-hot-toast'
+import React, { useState } from 'react'
 
-export default function RegisterForm() {
+export default function LogInForm() {
     const [formData, setFormData] = useState({
-        name: "",
         email: "",
         password: ""
     })
     const handleSubmit = async (event) => {
         event.preventDefault()
-        const result = await registerUser(formData)
-        if (result.error) {
-            toast.error(result.message)
-        }
-        if (result.data.acknowledged) {
-            toast.success(result.message)
-            setFormData({
-                email: "",
-                name: "",
-                password: ""
-            })
-        } else {
-            toast(result.message)
-        }
     }
+
     const handleChange = (event) => {
         const { name, value } = event.target;
-        setFormData((prev) => ({
-            ...prev,
-            [name]: value,
-        }));
+        setFormData((prev) => ({ ...prev, [name]: value }))
     }
     return (
         <div className="w-full border border-gray-200 rounded-lg p-8 shadow-sm bg-white">
-            <h2 className="text-2xl font-semibold text-center text-gray-800 mb-6">Sign Up</h2>
+            <h2 className="text-2xl font-semibold text-center text-gray-800 mb-6">Login</h2>
 
             <form onSubmit={handleSubmit} className="space-y-5">
-                <div>
-                    <label className="block text-sm font-medium text-gray-700 mb-1">Name</label>
-                    <input
-                        required
-                        value={formData.name}
-                        onChange={handleChange}
-                        type="text"
-                        name='name'
-                        placeholder="Your name"
-                        className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary-100"
-                    />
-                </div>
-
                 <div>
                     <label className="block text-sm font-medium text-gray-700 mb-1">Email</label>
                     <input
                         required
                         value={formData.email}
                         onChange={handleChange}
-                        type="email"
                         name='email'
+                        type="email"
                         placeholder="Your email"
-                        className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary-100"
+                        className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500"
                     />
                 </div>
 
@@ -71,8 +39,8 @@ export default function RegisterForm() {
                         required
                         value={formData.password}
                         onChange={handleChange}
-                        type="password"
                         name='password'
+                        type="password"
                         placeholder="Your password"
                         className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary-100"
                     />
@@ -82,7 +50,7 @@ export default function RegisterForm() {
                     type="submit"
                     className="w-full bg-primary-100 hover:bg-[#e63600] text-white font-semibold py-2 rounded-md transition-all cursor-pointer"
                 >
-                    Create Account
+                    Sign In
                 </button>
             </form>
 
@@ -95,13 +63,13 @@ export default function RegisterForm() {
                 <button className="w-10 h-10 rounded-full bg-gray-100 flex items-center justify-center text-blue-800 hover:shadow">
                     <i className="fab fa-linkedin-in"></i>
                 </button>
-                <button className="w-10 h-10 rounded-full bg-gray-100 flex items-center justify-center text-red-500 hover:shadow">
+                <button className="w-10 h-10 rounded-full bg-gray-100 flex items-center justify-center text-primary-100 hover:shadow">
                     <i className="fab fa-google"></i>
                 </button>
             </div>
 
             <div className="text-center text-sm text-gray-600">
-                Already have an account? <Link href={"/login"} className="text-primary-100 font-medium">Login</Link>
+                Don't have an account? <Link href={"/register"} className="text-primary-100 font-medium">Create Account</Link>
             </div>
         </div>
     )
--- src/app/(pages)/(authenticaton)/login/page.jsx
+++ src/app/(pages)/(authenticaton)/login/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import logInImage from "../../../../../public/assets/images/login/login.svg"
 import Image from 'next/image'
-import Link from 'next/link'
+import LogInForm from './components/LogInForm'
 
 export default function Login() {
     return (
@@ -10,54 +10,7 @@ export default function Login() {
                 <Image src={logInImage} height={500} width={460} alt='login image' />
             </div>
             <div className='col-span-1 w-full'>
-                <div className="w-full border border-gray-200 rounded-lg p-8 shadow-sm bg-white">
-                    <h2 className="text-2xl font-semibold text-center text-gray-800 mb-6">Login</h2>
-
-                    <form className="space-y-5">
-                        <div>
-                            <label className="block text-sm font-medium text-gray-700 mb-1">Email</label>
-                            <input
-                                type="email"
-                                placeholder="Your email"
-                                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500"
-                            />
-                        </div>
-
-                        <div>
-                            <label className="block text-sm font-medium text-gray-700 mb-1">Confirm Password</label>
-                            <input
-                                type="password"
-                                placeholder="Your password"
-                                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary-100"
-                            />
-                        </div>
-
-                        <button
-                            type="submit"
-                            className="w-full bg-primary-100 hover:bg-[#e63600] text-white font-semibold py-2 rounded-md transition-all"
-                        >
-                            Sign In
-                        </button>
-                    </form>
-
-                    <div className="my-6 text-center text-gray-500 text-sm">Or Sign In with</div>
-
-                    <div className="flex justify-center space-x-4 mb-4">
-                        <button className="w-10 h-10 rounded-full bg-gray-100 flex items-center justify-center text-blue-600 hover:shadow">
-                            <i className="fab fa-facebook-f"></i>
-                        </button>
-                        <button className="w-10 h-10 rounded-full bg-gray-100 flex items-center justify-center text-blue-800 hover:shadow">
-                            <i className="fab fa-linkedin-in"></i>
-                        </button>
-                        <button className="w-10 h-10 rounded-full bg-gray-100 flex items-center justify-center text-primary-100 hover:shadow">
-                            <i className="fab fa-google"></i>
-                        </button>
-                    </div>
-
-                    <div className="text-center text-sm text-gray-600">
-                        Don't have an account? <Link href={"/register"} className="text-primary-100 font-medium">Create Account</Link>
-                    </div>
-                </div>
+                <LogInForm />
             </div>
         </div>
     )
