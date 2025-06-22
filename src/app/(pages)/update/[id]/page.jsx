031d3afe2eb71fd32e4d2078196547964e7de418
lukmanxpert <lukmanmiah2004@gmail.com>
2025-06-22 23:20:20 +0600

    added validation in api

--- src/app/(pages)/update/[id]/page.jsx
+++ src/app/(pages)/update/[id]/page.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import UpdateBookingForm from '../components/UpdateForm';
+import { headers } from 'next/headers';
 
 export default async function UpdateBooking({ params }) {
   const p = await params;
-  const result = await fetch(`http://localhost:3000/api/my-bookings/${p.id}`)
+  const result = await fetch(`http://localhost:3000/api/my-bookings/${p.id}`, {
+    headers: await headers()
+  })
   const data = await result.json()
   return (
     <div>
--- src/app/api/my-bookings/[id]/route.js
+++ src/app/api/my-bookings/[id]/route.js
@@ -1,5 +1,7 @@
+import { authOptions } from "@/app/lib/authOptions";
 import dbConnect, { collectionNameObj } from "@/app/lib/dbConnect";
 import { ObjectId } from "mongodb";
+import { getServerSession } from "next-auth";
 import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 
@@ -7,26 +9,41 @@ export const GET = async (req, { params }) => {
   const p = await params;
   const bookingsCollection = dbConnect(collectionNameObj.bookingCollection);
   const query = { _id: new ObjectId(p.id) };
+  const session = await getServerSession(authOptions);
+  const email = session?.user?.email;
   const booking = await bookingsCollection.findOne(query);
-  return NextResponse.json(booking);
+  const isOwnerOk = email === booking?.email;
+  if (isOwnerOk) {
+    return NextResponse.json(booking);
+  } else {
+    return NextResponse.json({ message: "Forbidden access!" }, { status: 403 });
+  }
 };
 
 export const PATCH = async (req, { params }) => {
   const p = await params;
   const bookingsCollection = dbConnect(collectionNameObj.bookingCollection);
   const query = { _id: new ObjectId(p.id) };
-  const body = await req.json();
-  const updateDoc = {
-    $set: body,
-  };
-  const options = {
-    upsert: true,
-  };
-  const updateResponse = await bookingsCollection.updateOne(
-    query,
-    updateDoc,
-    options
-  );
-  revalidatePath("/my-bookings");
-  return NextResponse.json(updateResponse);
+  const session = await getServerSession(authOptions);
+  const email = session?.user?.email;
+  const currentBookings = await bookingsCollection.findOne(query);
+  const isOwnerOk = email === currentBookings?.email;
+  if (isOwnerOk) {
+    const body = await req.json();
+    const updateDoc = {
+      $set: body,
+    };
+    const options = {
+      upsert: true,
+    };
+    const updateResponse = await bookingsCollection.updateOne(
+      query,
+      updateDoc,
+      options
+    );
+    revalidatePath("/my-bookings");
+    return NextResponse.json(updateResponse);
+  } else {
+    return NextResponse.json({ message: "Forbidden access!" }, { status: 403 });
+  }
 };
